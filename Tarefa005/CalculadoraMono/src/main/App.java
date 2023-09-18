ba9213d185eb2aef3773d11ec53c42537a7217e7
Lucas Nunes Carvalho <lucasnc10@hotmail..com>
2023-09-17 23:30:36 -0300

    update monolitico mono

--- Tarefa005/CalculadoraMono/src/main/App.java
+++ Tarefa005/CalculadoraMono/src/main/App.java
@@ -3,30 +3,73 @@ import java.util.Scanner;
 
 import uteis.Calculadora;
 
-
 public class App {
     public static void main(String[] args){
         Calculadora calculadora = new Calculadora();
-        
         Scanner leitor = new Scanner(System.in);
+        int opcao = 0;
 
         double num1, num2;
 
-        System.out.println("Digite o primeiro número: ");
-        num1 = leitor.nextDouble();
+        while (opcao != 5) {
 
-        System.out.println("Digite o segundo número: \n");
-        num2 = leitor.nextDouble();
-        
-    try {        
-        System.out.println("Soma: " + calculadora.somar(num1,num2));
-        System.out.println("Subtração: " + calculadora.subtrair(num1, num2));
-        System.out.println("Multiplicação: "+ calculadora.multiplicar(num1 ,num2));
-        System.out.println("Divisão: " + calculadora.dividir(num1 ,num2));
-    } catch (Exception e) {
-      System.out.println("\n" + e);
-    }
+            System.out.println("\nEscolha uma operação");
+            System.out.println("\n 1 - Somar dois números:");
+            System.out.println("\n 2 - Subtrair dois números: ");
+            System.out.println("\n 3 - Multiplicar dois numeros:");
+            System.out.println("\n 4 - Dividir dois numeros:");
+            System.out.println("\n 5 - Encerrar progama\n");
 
+            System.out.println("\nDigite uma opção: ");
+            opcao = leitor.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("\nDigite o primeiro número: ");
+                    num1 = leitor.nextDouble();
+
+                    System.out.println("\nDigite o segundo número: ");
+                    num2 = leitor.nextDouble();
+
+                    System.out.println("\nSoma: " + calculadora.somar(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("\nDigite o primeiro número: ");
+                    num1 = leitor.nextDouble();
+
+                    System.out.println("\nDigite o segundo número: ");
+                    num2 = leitor.nextDouble();
+
+                    System.out.println("\nSubtração: " + calculadora.subtrair(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("\nDigite o primeiro número: ");
+                    num1 = leitor.nextDouble();
+
+                    System.out.println("\nDigite o segundo número: ");
+                    num2 = leitor.nextDouble();
+
+                    System.out.println("\nMultiplicação: " + calculadora.multiplicar(num1, num2));
+                    break;
+                case 4:
+                    try {
+                        System.out.println("\nDigite o primeiro número: ");
+                        num1 = leitor.nextDouble();
+
+                        System.out.println("\nDigite o segundo número: ");
+                        num2 = leitor.nextDouble();
+
+                        System.out.println("\nDivisão: " + calculadora.dividir(num1, num2));
+                    } catch (ArithmeticException e) {
+                        System.out.println("\n" + e);
+                    }
+                    break;
+                case 5: System.out.println("Programa encerrado\n"); 
+                    break;
+                default:
+                    System.out.println("Opcao invalida!");
+            }
+        }
         leitor.close();
 
     }
